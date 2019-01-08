b3ce4701a8a9f0b6132b8b65678984332b6d7642
Buyens Niels <niels.buyens@vito.be>
2019-01-08 12:32:03 +0100

    use python3 and spark2 and dynamic resource alloc
    Signed-off-by: Buyens Niels <niels.buyens@vito.be>

--- run-docker-sample.sh
+++ run-docker-sample.sh
@@ -2,3 +2,16 @@
 
 #spark-submit --master yarn-cluster --conf spark.shuffle.service.enabled=true --conf spark.dynamicAllocation.enabled=true docker.py
 spark-submit --master yarn-cluster --num-executors 10 --executor-memory 1g --driver-memory 1g docker-sample.py
+
+
+export HDP_VERSION=2.5.3.0-37
+export SPARK_MAJOR_VERSION=2
+export SPARK_HOME=/usr/hdp/current/spark2-client
+export PYSPARK_PYTHON="/usr/bin/python3.5"
+spark-submit --master yarn-cluster \
+--executor-memory 1g \
+--driver-memory 1g \
+--conf spark.shuffle.service.enabled=true --conf spark.dynamicAllocation.enabled=true \
+--conf spark.yarn.appMasterEnv.SPARK_HOME=/usr/hdp/current/spark2-client \
+--conf spark.yarn.appMasterEnv.PYSPARK_DRIVER_PYTHON=/usr/bin/python3.5 --conf spark.executorEnv.PYSPARK_PYTHON=/usr/bin/python3.5 \
+docker-sample.py
