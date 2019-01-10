63e0ce2d7c7d2542cc425bb18111c5abb0ca1744
Niels Buyens <niels.buyens@vito.be>
2019-01-10 11:34:09 +0100

    implement py-docker and try to catch signals to kill container
    Signed-off-by: Niels Buyens <niels.buyens@vito.be>

--- docker-sample.py
+++ docker-sample.py
@@ -1,9 +1,8 @@
 from pyspark import SparkContext
 import os 
 import subprocess
-#Install py-docker with pip install docker
-import docker
 import signal
+import docker
 
 """
 The code in the __main__ block will be executed on a single node, the 'driver'. It describes the different steps that need
@@ -16,8 +15,13 @@ if __name__ == '__main__':
 	#For bigger programs it's best to create a Dockerfile and build/run your docker container from that dockerfile.
 
         def handle_signal(signal, frame):
-            print("Killing executor and docker container now")
+            print("Caught signal %d Killing executor and docker container now" % signal)
             container.kill()
+
+        # catch all catchable signals
+        #catchable_sigs = set(signal.Signals) - {signal.SIGKILL, signal.SIGSTOP}
+        #for sig in catchable_sigs:
+        #    signal.signal(sig, handle_signal)
 
         # catch signals to update PID state when job is killed
         # trapping SIGKILL and SIGSTOP is not possible
@@ -27,18 +31,20 @@ if __name__ == '__main__':
         signal.signal(signal.SIGQUIT, handle_signal)
 
         client = docker.from_env()
+        print('starting docker container')
         container = client.containers.run("centos", "/bin/bash -c 'hostname; sleep 600'", detach=True)
         for line in container.logs(stream=True):
             print(line.strip())
-        return line
+        return str(container.id)
+
 	
     #The SparkContext is our entry point to bootstrap parallel operations.
     sc = SparkContext(appName='docker-sample')
 
     try:
-	#As we're not actually distributing anything we create a dummy list to distribute
-	programs_input = sc.parallelize (range(1,10))
-	#Run the dockerFunc on all executors and collect all the outputs in programs_output
+        #As we're not actually distributing anything we create a dummy list to distribute
+        programs_input = sc.parallelize(range(1,10))
+        #Run the dockerFunc on all executors and collect all the outputs in programs_output
         programs_output = programs_input.map(dockerFunc).collect()
         result = "".join(map(str,programs_output))
         print("The result is: " + result)
--- run-docker-sample.sh
+++ run-docker-sample.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#spark-submit --master yarn-cluster --conf spark.shuffle.service.enabled=true --conf spark.dynamicAllocation.enabled=true docker.py
-spark-submit --master yarn-cluster --num-executors 10 --executor-memory 1g --driver-memory 1g docker-sample.py
+#spark-submit --master yarn-cluster --conf spark.shuffle.service.enabled=true --conf spark.dynamicAllocation.enabled=true --executor-memory 1g --driver-memory 1g docker.py
+#spark-submit --master yarn-cluster --num-executors 10 --executor-memory 1g --driver-memory 1g docker-sample.py
 
 
-export HDP_VERSION=2.5.3.0-37
 export SPARK_MAJOR_VERSION=2
 export SPARK_HOME=/usr/hdp/current/spark2-client
 export PYSPARK_PYTHON="/usr/bin/python3.5"
@@ -15,3 +14,5 @@ spark-submit --master yarn-cluster \
 --conf spark.yarn.appMasterEnv.SPARK_HOME=/usr/hdp/current/spark2-client \
 --conf spark.yarn.appMasterEnv.PYSPARK_DRIVER_PYTHON=/usr/bin/python3.5 --conf spark.executorEnv.PYSPARK_PYTHON=/usr/bin/python3.5 \
 docker-sample.py
+
+
