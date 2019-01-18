6b6b87feed053cf3424245333337622c42ded0ee
Buyens Niels <niels.buyens@vito.be>
2019-01-18 10:18:16 +0100

    finished docker example
    Signed-off-by: Buyens Niels <niels.buyens@vito.be>

--- docker-sample.py
+++ docker-sample.py
@@ -5,7 +5,7 @@ Quick example docker run that returns the hostname of the docker container
 For bigger programs it's best to create a Dockerfile and build/run your docker container from that dockerfile.
 Make sure to always use the signal handler as otherwise docker containers will keep running when the application is killed or preempted by yarn.
 """
-def dockerFunc():
+def dockerFunc(f):
     import signal
     import docker
 
@@ -22,7 +22,7 @@ def dockerFunc():
 
     client = docker.from_env()
     # Detach the container as it will be running in the background.
-    container = client.containers.run("centos:7", "/bin/bash -c 'hostname'", detach=True)
+    container = client.containers.run("centos:7", "/bin/bash -c 'hostname; echo " + str(f) + "; sleep 5;'", detach=True)
     # This will attach to the docker log output and will log this in the Spark executor logs
     for line in container.logs(stream=True):
         print(line.strip())
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     sc = SparkContext(appName='docker-sample')
     try:
         # As this is just an example there's no input to distribute, so we just parallelize over a range
-        sc.parallelize(range(1,5)).foreach(lambda f : dockerFunc())
+        sc.parallelize(range(1,5)).foreach(lambda f : dockerFunc(f))
     finally:
         sc.stop()
 
